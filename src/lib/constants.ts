e656ab4dc07cdee0bd8f03ad5485108034dd4e82
Gojo <vishalvc16vc@gmail.com>
2026-08-02 15:48:27 +0530

    Add constants for ShelbyUSD fungible asset

---
+++ src/lib/constants.ts
@@ -0,0 +1,3 @@
+// The ShelbyUSD fungible asset metadata address on Shelbynet.
+export const SHELBY_USD_FA_ADDRESS = "0x1b18363a9f1fe5e6ebf247daba5cc1c18052bb232efdc4c50f556053922d98e1";
+export const SHELBY_USD_DECIMALS = 1_000_000; // 6 decimal places
